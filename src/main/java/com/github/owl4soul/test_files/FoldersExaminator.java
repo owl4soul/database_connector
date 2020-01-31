0ad93885e9b66dbcbe84ae1250b31363c1634b85
owl4soul <resuuser989@gmail.com>
2020-01-31 18:37:10 +0300

    Реализован новый алгоритм - теперь в качестве аргумента командной строки ожидается путь к корневой директории, в которой находятся папки, а внутри них еще папки (целевые) с файлами.
    Выводится необходимая инфа о названиях файлов и количестве.

--- src/main/java/com/github/owl4soul/test_files/FoldersExaminator.java
+++ src/main/java/com/github/owl4soul/test_files/FoldersExaminator.java
@@ -5,6 +5,7 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -18,42 +19,49 @@ import java.util.Set;
 public class FoldersExaminator {
 
 
-	private static Map<File, File[]> mappedDirectories;
+	/**
+	 * Путь к корню, папки внутри которого мы будем рассматривать.
+	 * Передается в качестве аргумента.
+	 */
+	private static String rootFolderPath;
+
+	/**
+	 * Размапленные папки внутри переданной в качестве аргумента корневой директории.
+	 */
+	private static Map<File, File[]> mappedTargetFolders;
 
 	private static Set<File> equalByNameFiles;
 
 	private static Set<File> notEqualByName;
 
-	private static List<File> allFolders = new ArrayList<>();
+	private static List<File> allTargetFolders = new ArrayList<>();
 
 	private static List<File> differentByContentFromEqualByNameFiles = new ArrayList<>();
 
 	public static void main(String... paths) throws IOException {
-		mappedDirectories = new HashMap<>();
+		// Принимаем аргумент, который должен указать корневую директорию с папками, которые будем проверять
+		rootFolderPath = paths[0];
 
-		for (String path : paths) {
+		// Получение всех путей к целевым папкам - по задаче внутри рутовой папки хранятся файлы через двойную вложенность
+		initAllTargetFolders();
 
-			// Получение всех файлов в папке
-			File folder = new File(path);
-			allFolders.add(folder);
-			File[] filesInFolder = folder.listFiles();
-			mappedDirectories.put(folder, filesInFolder);
-		}
+		// Маппинг целевых папок - ключ: целевая папка, значение: массив файлов.
+		initFilesPerTargetFoldersMap();
 
 		equalByNameFiles = new HashSet<>();
 		notEqualByName = new HashSet<>();
 
-		List<File> foldersList = new ArrayList<>(mappedDirectories.keySet());
+		List<File> foldersList = new ArrayList<>(mappedTargetFolders.keySet());
 
 		for (int i = 0; i < foldersList.size() - 1; i++) {
 			// сравниваем этот каталог со следующим, находим файлы с одинаквыми названиями и суем в сет
-			addSameNamedFilesToSet(foldersList.get(i), foldersList.get(i + 1));
+			addSameNamedFilesToSet(i, foldersList.get(i), foldersList.get(i + 1));
 		}
 
 		for (File commonFile : equalByNameFiles) {
 			List<File> filesToCheck = new ArrayList<>();
 
-			for (Map.Entry<File, File[]> mappedFolderEntry : mappedDirectories.entrySet()) {
+			for (Map.Entry<File, File[]> mappedFolderEntry : mappedTargetFolders.entrySet()) {
 				File[] filesInFolder = mappedFolderEntry.getValue();
 				for (File fileInFolder : filesInFolder) {
 					if (fileInFolder.getName().equals(commonFile.getName())) {
@@ -72,6 +80,7 @@ public class FoldersExaminator {
 			}
 		}
 
+		System.out.println("Not equal named COUNT = " + notEqualByName.size());
 		System.out.println("Not equal named files: ");
 		if (notEqualByName.isEmpty()) {
 			System.out.println("NOT FOUND");
@@ -83,8 +92,8 @@ public class FoldersExaminator {
 
 		System.out.println("--------------------------------------------------------------");
 
+		System.out.println("Equal named COUNT = " + equalByNameFiles.size());
 		System.out.println("Equal named files: ");
-
 		if (equalByNameFiles.isEmpty()) {
 			System.out.println("NOT FOUND");
 		} else {
@@ -95,6 +104,7 @@ public class FoldersExaminator {
 
 		System.out.println("--------------------------------------------------------------");
 
+		System.out.println("Files with different content from equal by name COUNT = " + differentByContentFromEqualByNameFiles.size());
 		System.out.println("Files with different content from equal by name files: ");
 		if (differentByContentFromEqualByNameFiles.isEmpty()) {
 			System.out.println("NOT FOUND");
@@ -104,6 +114,38 @@ public class FoldersExaminator {
 			}
 		}
 
+		System.out.println("--------------------------------------------------------------");
+
+		Set<String> namesOfDifferentContentFiles = new HashSet<>();
+		differentByContentFromEqualByNameFiles.forEach(file -> namesOfDifferentContentFiles.add(file.getName()));
+		System.out.println("Different NAMES of same named files with different content COUNT = " + namesOfDifferentContentFiles.size());
+		System.out.println("Different NAMES of same named files with different content: ");
+		for (String nameOfFile :  namesOfDifferentContentFiles) {
+			System.out.println(nameOfFile);
+		}
+	}
+
+	/**
+	 * Инициализация всех целевых папок, внутри которых будем проверять файлы на уникальность контента.
+	 */
+	private static void initAllTargetFolders() {
+		List<File> rootFoldersContent = Arrays.asList(new File(rootFolderPath).listFiles());
+
+		for (File contentInRoot : rootFoldersContent) {
+			if (contentInRoot.isDirectory()){
+				allTargetFolders.addAll(Arrays.asList(contentInRoot.listFiles()));
+			}
+		}
+	}
+
+	/**
+	 * Заполнение мапы mappedTargetFolders - где ключ: целевая папка, значение: массив файлов в ней.
+	 */
+	private static void initFilesPerTargetFoldersMap() {
+		mappedTargetFolders = new HashMap<>();
+		for (File targetFolder : allTargetFolders) {
+			mappedTargetFolders.put(targetFolder, targetFolder.listFiles());
+		}
 	}
 
 	public static boolean checkIfContentsAreIdentical(File file1, File file2) throws IOException {
@@ -137,47 +179,28 @@ public class FoldersExaminator {
 		return stringBuilder.toString();
 	}
 
-	public static void addSameNamedFilesToSet(File folder1, File folder2) {
-		// достанем из мапы все файлы данного каталога
-//		List<File> folder1Files = Arrays.asList(mappedDirectories.get(folder1));
-//		List<File> folder2Files = Arrays.asList(mappedDirectories.get(folder2));
+	public static void addSameNamedFilesToSet(int step, File folder1, File folder2) {
+		System.out.println("----on step " + step + " checking folders: " + folder1.getParent() + " & " + folder2.getParent());
 
+		// достанем из мапы все файлы данного каталога
 		Set<File> folder1Files = new HashSet<>();
 		Set<File> folder2Files = new HashSet<>();
 
-		Collections.addAll(folder1Files, mappedDirectories.get(folder1));
-		Collections.addAll(folder2Files, mappedDirectories.get(folder2));
+		Collections.addAll(folder1Files, mappedTargetFolders.get(folder1));
+		Collections.addAll(folder2Files, mappedTargetFolders.get(folder2));
 
 		folder1Files.retainAll(folder2Files);
 
 		notEqualByName.addAll(folder1Files);
 
 		Set<File> folder1FromMap = new HashSet<>();
-		Collections.addAll(folder1FromMap, mappedDirectories.get(folder1));
+		Collections.addAll(folder1FromMap, mappedTargetFolders.get(folder1));
 
 		folder1FromMap.removeAll(folder1Files);
 
 		equalByNameFiles.addAll(folder1FromMap);
 
 	}
-
-//	File f = new File (((File)foldersList.get(0)).listFiles()[0].getAbsolutePath());
-//
-//	StringBuilder stringBuilder = new StringBuilder();
-//
-//	FileReader fr = new FileReader(f);
-//	//создаем BufferedReader с существующего FileReader для построчного считывания
-//	BufferedReader reader = new BufferedReader(fr);
-//	// считаем сначала первую строку
-//	String line = reader.readLine();
-//while (line != null) {
-//		System.out.println(line);
-//		stringBuilder.append(line);
-//		// считываем остальные строки в цикле
-//		line = reader.readLine();
-//	}
-//
-//stringBuilder.toString()
 }
 
 
