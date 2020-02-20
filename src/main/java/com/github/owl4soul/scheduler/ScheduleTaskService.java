63e05ea287ab204499e7ba7cc63732cc9d7f3b1d
owl4soul <resuuser989@gmail.com>
2020-02-20 12:19:34 +0300

    Реализация удаления запланированной задачи (чтоб она перестала отрабатывать сразу и удалилась из сохраняемых задач, которые будут загружаться при обновлении контекста в дальнейшем).

--- src/main/java/com/github/owl4soul/scheduler/ScheduleTaskService.java
+++ src/main/java/com/github/owl4soul/scheduler/ScheduleTaskService.java
@@ -7,10 +7,8 @@ import org.springframework.scheduling.support.CronTrigger;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-import java.time.Instant;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.TimeZone;
 import java.util.concurrent.ScheduledFuture;
 
 /**
@@ -27,16 +25,18 @@ public class ScheduleTaskService {
 	Map<String, ScheduledFuture<?>> scheduledFutureMap = new HashMap<>();
 
 	// Добавление задачи в мапу запланированных задач
-	public void addTaskToScheduler(String id, Runnable task) {
-		ScheduledFuture<?> scheduledTask = taskScheduler.schedule(task, new CronTrigger("0 0 0 * * ?", TimeZone
-				.getTimeZone(TimeZone.getDefault().getID())));
-		scheduledFutureMap.put(id, scheduledTask);
-	}
-
-	// Добавление задачи в мапу запланированных задач
 	public void addTaskToScheduler(String id, Runnable task, CronTrigger cronTrigger) {
 		ScheduledFuture<?> scheduledTask = taskScheduler.schedule(task, cronTrigger);
 		scheduledFutureMap.put(id, scheduledTask);
+	}
+
+	// Удаление запланированной задачи
+	public void removeTaskFromScheduler(String id) {
+		ScheduledFuture<?> scheduledTask = scheduledFutureMap.get(id);
+		if(scheduledTask != null) {
+			scheduledTask.cancel(true);
+			scheduledFutureMap.remove(id);
+		}
 	}
 
 	// Слушатель обновления контекста
@@ -45,15 +45,21 @@ public class ScheduleTaskService {
 		// Здесь надо загрузить из дб таски в мапу.
 		System.out.println("Context has been refreshed");
 
-		Runnable task = new Taska();
+		// Здесь мы считываем сохраненные параметры запланированных задач:
+		//  - id (не обязательно),
+		//  - cron-выражение и/или некоторые параметры, указывающие на время и интервал и т.п.
 
-		addTaskToScheduler("Test_task_at_" + Instant.now(), task, new CronTrigger("*/10 * * * * *"));
+		// Затем по полученным данным инициализируем все задачи заново, как здесь:
+		Runnable task = new Taska();
+		addTaskToScheduler("Test_task_at", task, new CronTrigger("*/10 * * * * *"));
 	}
 
-	private static class Taska implements Runnable {
+	private class Taska implements Runnable {
 		@Override
 		public void run() {
 			System.out.println("Я - таска, сработавшая по расписанию!");
+			removeTaskFromScheduler("Test_task_at");
+			System.out.println("Я - таска, решившая отменить себя, больше не сработаю!");
 		}
 	}
 
